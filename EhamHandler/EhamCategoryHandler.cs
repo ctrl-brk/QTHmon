bdff61365f0a198f2f390086422cca40d53758d0
Alex <aleksey_grachev@yahoo.com>
2023-06-12 20:03:45 -0600

    sendgrid - closes #38 (#39)

--- EhamHandler/EhamCategoryHandler.cs
+++ EhamHandler/EhamCategoryHandler.cs
@@ -1,69 +1,57 @@
-﻿using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
+﻿// ReSharper disable once CheckNamespace
+namespace QTHmon;
 
-// ReSharper disable once CheckNamespace
-namespace QTHmon
+public partial class EhamHandler
 {
-    public partial class EhamHandler
+    public async Task<IEnumerable<ScanResult>> ProcessCategoriesAsync(HttpClient httpClient, CookieContainer cookies, CancellationToken token)
     {
-        public async Task<IEnumerable<ScanResult>> ProcessCategoriesAsync(HttpClient httpClient, CookieContainer cookies, CancellationToken token)
-        {
-            if (_settings.EhamNet.CategorySearch.MaxPosts <= 0) return null;
+        if (_settings.EhamNet.CategorySearch.MaxPosts <= 0) return null;
 
-            _thisScan = new ScanInfo { Ids = new List<int>() };
+        _thisScan = new ScanInfo { Ids = new List<int>() };
 #if DEBUG && CLEARHIST
             File.Delete(_settings.EhamNet.CategorySearch.ResultFile);
 #endif
-            if (File.Exists(_settings.EhamNet.CategorySearch.ResultFile))
-            {
-                _lastCategoryScan = JsonConvert.DeserializeObject<ScanInfo>(File.ReadAllText(_settings.EhamNet.CategorySearch.ResultFile));
-                _lastCategoryScan.OtherIds = new List<int>(_lastKeywordScan.Ids);
-            }
-
-            var res = new List<ScanResult>();
-
-            foreach (var category in _settings.EhamNet.CategorySearch.Categories.Split(','))
-            {
-                if (token.IsCancellationRequested) break;
-                _newPosts = new List<Post>();
-                res.Add(await ProcessCategory(httpClient, category, cookies, token));
-            }
-
-            return res;
+        if (File.Exists(_settings.EhamNet.CategorySearch.ResultFile))
+        {
+            _lastCategoryScan = JsonConvert.DeserializeObject<ScanInfo>(await File.ReadAllTextAsync(_settings.EhamNet.CategorySearch.ResultFile, token));
+            _lastCategoryScan.OtherIds = new List<int>(_lastKeywordScan.Ids);
         }
 
-        private async Task<ScanResult> ProcessCategory(HttpClient httpClient, string category, CookieContainer cookies, CancellationToken token)
+        var res = new List<ScanResult>();
+
+        foreach (var category in _settings.EhamNet.CategorySearch.Categories.Split(','))
         {
-            _logger.LogDebug($"Fetching {category} category from eHam.net");
+            if (token.IsCancellationRequested) break;
+            _newPosts = new List<Post>();
+            res.Add(await ProcessCategory(httpClient, category, cookies, token));
+        }
 
-            var uri = new Uri($"https://www.eham.net/classifieds/view-category?id={_categories.First(x => x.Key == category.ToLower()).Value}");
+        return res;
+    }
 
-            var sessionCookie = await GetSessionCookie(httpClient, cookies);
+    private async Task<ScanResult> ProcessCategory(HttpClient httpClient, string category, CookieContainer cookies, CancellationToken token)
+    {
+        _logger.LogDebug("Fetching {Category} category from eHam.net", category);
 
-            var message = new HttpRequestMessage(HttpMethod.Get, uri);
-            message.Headers.Add("Cache-Control", "no-cache");
-            message.Headers.Add("Cookie", $"{sessionCookie.Name}={sessionCookie.Value}");
+        var uri = new Uri($"https://www.eham.net/classifieds/view-category?id={_categories.First(x => x.Key == category.ToLower()).Value}");
 
-            var res = await httpClient.SendAsync(message);
-            if (token.IsCancellationRequested) return null;
-            var msg = await res.Content.ReadAsStringAsync();
-            if (!await ScanResults(msg, ScanType.Category, httpClient)) return null;
+        var sessionCookie = await GetSessionCookie(httpClient, cookies);
 
-            _newPosts.ForEach(x => _thisScan.Ids.Add(x.Id));
+        var message = new HttpRequestMessage(HttpMethod.Get, uri);
+        message.Headers.Add("Cache-Control", "no-cache");
+        message.Headers.Add("Cookie", $"{sessionCookie.Name}={sessionCookie.Value}");
+
+        var res = await httpClient.SendAsync(message, token);
+        if (token.IsCancellationRequested) return null;
+        var msg = await res.Content.ReadAsStringAsync(token);
+        if (!await ScanResults(msg, ScanType.Category, httpClient)) return null;
+
+        _newPosts.ForEach(x => _thisScan.Ids.Add(x.Id));
 
 #if !DEBUG || SAVEHIST
             if (_newPosts.Count > 0)
-                File.WriteAllText(_settings.EhamNet.CategorySearch.ResultFile, JsonConvert.SerializeObject(_thisScan));
+                await File.WriteAllTextAsync(_settings.EhamNet.CategorySearch.ResultFile, JsonConvert.SerializeObject(_thisScan), token);
 #endif
-            return BuildResults(_lastCategoryScan);
-        }
+        return BuildResults(_lastCategoryScan);
     }
 }
